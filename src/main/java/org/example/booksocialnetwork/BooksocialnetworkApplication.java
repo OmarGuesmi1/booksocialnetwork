c18eb6c80fd6381ce0f3f0fcedf65d1483e374b1
OMARog0123 <omar.guesmi@esprit.tn>
2025-06-15 17:05:14 +0100

    bsn project

---
+++ src/main/java/org/example/booksocialnetwork/BooksocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksocialnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksocialnetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/booksocialnetwork/BooksocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booksocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksocialnetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
